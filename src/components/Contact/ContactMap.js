bafa14d8175e1513fb6ee9f86ceed909824af81b
Olabiran Joshua <olabiranj@gmail.com>
2020-05-05 08:42:42 +0100

    landing page done

--- src/components/Contact/ContactMap.js
+++ src/components/Contact/ContactMap.js
@@ -4,7 +4,7 @@ const ContactMap = () => {
   return (
     <iframe
       title="Andela, Nigeria."
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.785387052263!2d3.2345080147708405!3d6.548759595263443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b853fa37c1177%3A0x477d07f40cef86a1!2sOlorunfemi%20Bus%20station!5e0!3m2!1sen!2sng!4v1582307168526!5m2!1sen!2sng"
+      src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d30585.49408227924!2d3.366675750650524!3d6.555627266071869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sandela%20lagos%20map!5e0!3m2!1sen!2sng!4v1588664445446!5m2!1sen!2sng"
       width="600"
       height="450"
       frameBorder="0"
