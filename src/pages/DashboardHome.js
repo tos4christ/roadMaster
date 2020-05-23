ced3b3edc5c4b3745ead1b12b6c95941f256d48b
Olabiran Joshua <olabiranj@gmail.com>
2020-05-23 11:11:07 +0100

    dashboard added

--- src/pages/DashboardHome.js
+++ src/pages/DashboardHome.js
@@ -1,21 +1,14 @@
 import React from "react";
 import DashboardNav from '../components/Header/DashboardNav';
+import AccidentAreas from '../components/Charts/AccidentAreas';
+import AccidentRep from '../components/Charts/AccidentsRep';
 
 const DashboardHome = () => {
   const body = <div className='container-fluid' >
     <div className="row my-4 pt-4 text-justify">
       <div className="col-sm-4">
         <div className='bg-white shadow m-1 p-3'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicin
-          g elit. Eligendi, vitae reprehenderit nulla, pariatur provident d
-          ignissimos distinctio quaerat quos, libero placeat ex nobis quas corrupt
-          i adipisci quia sunt perspiciatis quod. Iste molestias nobis corrupti tempore
-          architecto dolorem, recusandae odio eum quibusdam! Inventore optio mollitia quis,
-          neque omnis, qui molestiae aliquid debitis obcaecati itaque repellendus
-          voluptatibus rerum. Aut minus ducimus enim et nam rem accusamus perspiciatis
-          eos ab praesentium consequatur ea, unde beatae voluptatum corrupti. Dolores quae
-          nobis magnam labore, atque consequatur pariatur quia delectus! Ex necessitatibus
-          nesciunt saepe deleniti nam nulla excepturi reiciendis fuga rerum quam quis sed.</p>
+          <AccidentAreas/>
         </div>
         <div className="row mt-4">
           <div className="col-sm-6 ">
@@ -31,21 +24,7 @@ const DashboardHome = () => {
           </div>
       </div>
       <div className="col-sm-8">
-        <div className='bg-white shadow m-1 p-3'><p>Lorem ipsum dolor sit amet consectetur adipisicin
-        g elit. Eligendi, vitae reprehenderit nulla, pariatur provident d
-        ignissimos distinctio quaerat quos, libero placeat ex nobis quas corrupt
-        i adipisci quia sunt perspiciatis quod. Iste molestias nobis corrupti tempore
-        architecto dolorem, recusandae odio eum quibusdam! Inventore optio mollitia quis,
-        neque omnis, qui molestiae aliquid debitis obcaecati itaque repellendus
-        voluptatibus rerum. Aut minus ducimus enim et nam rem accusamus perspiciatis
-        eos ab praesentium consequatur ea, unde beatae voluptatum corrupti. Dolores quae
-        nobis magnam labore, atque consequatur pariatur quia delectus! Ex necessitatibus
-        blanditiis exercitationem? Itaque asperiores a error, aspernatur rem veritatis dolor,
-        quod nam modi amet fugiat. Ea provident officia obcaecati ipsam impedit. Unde quos quibusdam
-        quis beatae sapiente alias nihil totam tenetur quod inventore sequi possimus aliquid,
-        incidunt cum. Rem est esse harum autem aliquam consequatur illum eius? Accusantium
-        nesciunt saepe deleniti nam nulla excepturi reiciendis fuga rerum quam quis sed,
-        quidem minima praesentium molestias, debitis vitae maxime alias. Culpa placeat.</p></div>
+        <div className='bg-white shadow m-1 p-3'><AccidentRep/></div>
         <div className="row mt-4">
           <div className="col-sm-6 ">
             <div className="bg-white shadow m-1 p-3"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam quas, non molestias voluptate
