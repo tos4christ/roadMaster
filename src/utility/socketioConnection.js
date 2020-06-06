c669eba7bffd66136f7fc90216241af816d72e38
tos4christ <gnztrades@gmail.com>
2020-06-06 20:14:50 +0100

    updates

--- src/utility/socketioConnection.js
+++ src/utility/socketioConnection.js
@@ -1,6 +1,9 @@
 import openSocket from 'socket.io-client';
  // start the client socket
- const socket = openSocket('http://localhost:5000/');
+  // Local Server
+ // const socket = openSocket('http://localhost:5000/');
+    // Heroku Server
+ const socket = openSocket('https://covid-19-tos4christ.herokuapp.com/'); 
 
  export default socket;
  
